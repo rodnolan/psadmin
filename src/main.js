ac96e730b99f06d822ff7d617325bf3ca96e0e78
Rod Nolan <rodnolan@gmail.com>
2016-03-27 11:31:26 -0400

    completed demo
    added an About component and added a rudimentary, "no library" routing
    solution

--- src/main.js
+++ src/main.js
@@ -1,5 +1,36 @@
 $ = jQuery = require('jquery');
 var React = require('react');
 var HomePage = require('./components/homePage');
+var About = require('./components/about/aboutPage');
 
-React.render(<HomePage />, document.getElementById('app'));
+(function(win){
+	"use strict";
+
+	var App = React.createClass({
+		render: function () {
+			var Child;
+			switch (this.props.route) {
+				case 'about':
+					Child = About;
+					break;
+				default: 
+					Child = HomePage;
+					break;
+			}
+			return (
+				<div>
+					<Child />
+				</div>
+			);
+
+		}
+	});
+
+	function render() {
+		var route = win.location.hash.substr(1);
+		React.render(<App route={route} />, document.getElementById('app'));	
+	}
+	win.addEventListener('hashchange', render);
+	render();
+
+})(window);
