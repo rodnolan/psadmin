67a0d73ac583df53eb0749fba8e4c93f293db0ec
Rod Nolan <rodnolan@gmail.com>
2016-03-28 10:44:32 -0400

    added author page to display mock data
    getInitialState sets a default value for authors
    componentWillMount gets the author data from the mock API and assigns it
    to the 'authors' state variable

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ var React = require('react');
 var Home = require('./components/homePage');
 var About = require('./components/about/aboutPage');
 var Header = require('./components/common/header');
+var Authors = require('./components/authors/authors');
 
 (function(win){
 	"use strict";
@@ -13,6 +14,9 @@ var Header = require('./components/common/header');
 			switch (this.props.route) {
 				case 'about':
 					Child = About;
+					break;
+				case 'authors':
+					Child = Authors;
 					break;
 				default: 
 					Child = Home;
