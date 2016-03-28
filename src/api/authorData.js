24d2f168f7d783dfce55eefa7cd34ea441a1924c
Rod Nolan <rodnolan@gmail.com>
2016-03-28 10:41:46 -0400

    completed setup
    added lodash for use in the mock api and also added some hard coded data

---
+++ src/api/authorData.js
@@ -0,0 +1,20 @@
+module.exports = {
+	authors: 
+	[
+		{
+			id: 'rod-nolan', 
+			firstName: 'Rod', 
+			lastName: 'Nolan'
+		},	
+		{
+			id: 'gord-downey', 
+			firstName: 'Gord', 
+			lastName: 'Downey'
+		},	
+		{
+			id: 'ben-harper', 
+			firstName: 'Ben', 
+			lastName: 'Harper'
+		}
+	]
+};
