f3324f1369279b4b856358790c46d7189eda3915
Rod Nolan <rodnolan@gmail.com>
2016-03-28 23:12:18 -0400

    replaced <a href=""> with Link

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,6 +1,8 @@
 "use strict";
 
 var React = require('react');
+var Router = require('react-router');
+var Link = Router.Link;
 
 var HomePage = React.createClass({
 
@@ -10,6 +12,7 @@ var HomePage = React.createClass({
 			<div className="jumbotron"> 
 				<h1>Pluralsight Administration</h1>
 				<p>React, React Router and Flux for ultra-responsive web apps.</p>
+				<Link to="about" className="btn- btn-primary btn-lg">Learn More</Link>
 			</div>
 		);
 	}
