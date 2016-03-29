9e671e5194d15d84c0bc5e77bc6ea2e26ff53d2c
Rod Nolan <rodnolan@gmail.com>
2016-03-29 09:35:40 -0400

    created parent controller view (manageAuthor), child (authorForm) and reusable text input component

---
+++ src/components/authors/authorForm.js
@@ -0,0 +1,42 @@
+"use strict";
+
+var React = require('react');
+var TextInput = require('../common/textInput');
+
+var AuthorForm = React.createClass({
+	
+	propTypes: {
+		author: React.PropTypes.object.isRequired,
+		onChange: React.PropTypes.func.isRequired
+	},
+	
+	render: function() {
+		return (
+			<form>
+				<TextInput
+					name="firstName"
+					label="First Name"
+					value={this.props.author.firstName}
+					onChange={this.props.onChange} 
+					placeholder="First Name"
+				/>
+				
+				<TextInput
+					name="lastName"
+					label="Last Name"
+					value={this.props.author.lastName}
+					onChange={this.props.onChange} 
+					placeholder="Last Name"
+				/>
+
+				<input type="submit"
+					value="Save"
+					className="btn btn-default"
+				/>
+			</form>
+
+		);
+	}
+});
+
+module.exports = AuthorForm;
--- src/components/authors/manageAuthor.js
+++ src/components/authors/manageAuthor.js
@@ -1,13 +1,32 @@
 "use strict";
 
 var React = require('react');
+var AuthorForm = require('./authorForm');
 
 var ManageAuthorPage = React.createClass({
+	getInitialState: function() {
+		return {
+			author: {id: '', firstName: '', lastName: ''}
+		};
+	},
+	
+	setAuthorState: function(event) {
+		// update state in parent component based on 
+		// events that bubble up from child component
+		var field = event.target.name;
+		var value = event.target.value;
+		this.state.author[field] = value;
+		return this.setState( {author: this.state.author} );
+	},
 
 	render: function() {
 		return (
 			<div>
 				<h1>Manage Author</h1>
+				<AuthorForm 
+					author={this.state.author}
+					onChange={this.setAuthorState}
+				/>
 			</div>
 
 		);
