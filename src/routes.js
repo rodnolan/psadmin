3a040734f1347a9713546aa7ae8549ff981f98c5
Rod Nolan <rodnolan@gmail.com>
2016-03-29 08:01:37 -0400

    handling URL changes
    type /#/about-us and watch the browser redirect to /#/about

--- src/routes.js
+++ src/routes.js
@@ -6,12 +6,15 @@ var Router = require('react-router');
 var DefaultRoute = Router.DefaultRoute;
 var Route = Router.Route;
 var NotFoundRoute = Router.NotFoundRoute;
+var Redirect = Router.Redirect;
 
 var routes = (
 	<Route name="app" path="/" handler={require('./components/app')}>
 		<DefaultRoute handler={require('./components/homePage')}></DefaultRoute>
 		<Route name="authors" handler={require('./components/authors/authors')}></Route>
 		<Route name="about" handler={require('./components/about/aboutPage')}></Route>
+		<Redirect from="about-us" to="about" />
+
 		<NotFoundRoute handler={require('./components/notFoundPage')}></NotFoundRoute>
 	</Route>
 );
